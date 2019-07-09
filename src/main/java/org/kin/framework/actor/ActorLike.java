56a01c4716e5325ec45fad56bd45c032f44c9c0c
huangjianqin <18814127639@163.com>
2019-07-09 20:14:21 +0800

    增加简单的类actor实现

---
+++ src/main/java/org/kin/framework/actor/ActorLike.java
@@ -0,0 +1,170 @@
+package org.kin.framework.actor;
+
+import org.kin.framework.JvmCloseCleaner;
+import org.kin.framework.actor.domain.ActorPath;
+import org.kin.framework.concurrent.SimpleThreadFactory;
+import org.kin.framework.concurrent.ThreadManager;
+import org.kin.framework.utils.SysUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author huangjianqin
+ * @date 2019/7/9
+ */
+public abstract class ActorLike<AL extends ActorLike<?>> implements Actor<AL>, Runnable {
+    private static final Logger log = LoggerFactory.getLogger("actor");
+    private static final Logger profileLog = LoggerFactory.getLogger("actorProfile");
+
+    private static final ThreadManager THREADS = new ThreadManager(
+            new ThreadPoolExecutor(0, SysUtils.getSuitableThreadNum() * 2 - 1,
+                    60L, TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<>(),
+                    new SimpleThreadFactory("actorlike")),
+            new ScheduledThreadPoolExecutor(SysUtils.getSuitableThreadNum() * 2 - 1,
+                    new SimpleThreadFactory("actorlike-schedule")));
+    private static Map<ActorLike<?>, Queue<Future>> futures = new ConcurrentHashMap<>();
+    static {
+        JvmCloseCleaner.DEFAULT().add(() -> {
+            THREADS.shutdown();
+        });
+    }
+
+    private final Queue<Message<AL>> messageBox = new LinkedBlockingDeque<>();
+    private final AtomicInteger boxSize = new AtomicInteger();
+    private volatile Thread currentThread;
+    private volatile boolean isStopped = false;
+
+    public ActorLike() {
+        //每1h清楚已结束的调度
+        scheduleAtFixedRate(actor -> clearFinishedFutures(), 0, 1, TimeUnit.HOURS);
+    }
+
+    @Override
+    public <T> void receive(T message) {
+        if(!isStopped){
+            if(message instanceof Message){
+                messageBox.add((Message<AL>) message);
+                tryRun();
+            }
+            else{
+                throw new UnsupportedOperationException();
+            }
+        }
+    }
+
+    @Override
+    public void tell(Message<AL> message) {
+        if(!isStopped){
+            messageBox.add(message);
+            tryRun();
+        }
+    }
+
+    @Override
+    public Future<?> schedule(Message<AL> message, long delay, TimeUnit unit) {
+        if(!isStopped){
+            Future future = THREADS.schedule(() -> {
+                receive(message);
+            }, delay, unit);
+            addFuture(future);
+            return future;
+        }
+        return null;
+    }
+
+    @Override
+    public Future<?> scheduleAtFixedRate(Message<AL> message, long initialDelay, long period, TimeUnit unit) {
+        if(!isStopped){
+            Future future = THREADS.scheduleAtFixedRate(() -> {
+                receive(message);
+            }, initialDelay, period, unit);
+            addFuture(future);
+            return future;
+        }
+        return null;
+    }
+
+    @Override
+    public void stop() {
+        stopNow();
+    }
+
+    @Override
+    public void stopNow() {
+        if(!isStopped){
+            isStopped = true;
+            clearFutures();
+        }
+    }
+
+    @Override
+    public ActorPath getPath() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void run() {
+        this.currentThread = Thread.currentThread();
+        while (!isStopped && !this.currentThread.isInterrupted()) {
+            Message message = messageBox.poll();
+            if (message == null) {
+                break;
+            }
+
+            long st = System.currentTimeMillis();
+            message.handle(this);
+            long cost = System.currentTimeMillis() - st;
+
+            profileLog.info("handle mail({}) cost {} ms", message, cost);
+
+            if (boxSize.decrementAndGet() <= 0) {
+                break;
+            }
+        }
+        this.currentThread = null;
+    }
+
+    private void tryRun() {
+        if (!isStopped && boxSize.incrementAndGet() == 1) {
+            THREADS.execute(this);
+        }
+    }
+
+    private void addFuture(Future<?> future) {
+        Queue<Future> queue;
+        while ((queue = futures.putIfAbsent(this, new ConcurrentLinkedQueue<>())) == null) {
+        }
+        queue.add(future);
+    }
+
+    private void clearFutures() {
+        Queue<Future> old = futures.remove(this);
+        if (old != null) {
+            for (Future future : old) {
+                if (!future.isDone() || !future.isCancelled()) {
+                    future.cancel(true);
+                }
+            }
+        }
+    }
+
+    private void clearFinishedFutures() {
+        Queue<Future> old = futures.get(this);
+        if (old != null) {
+            Iterator<Future> iterator = old.iterator();
+            while (iterator.hasNext()) {
+                Future future = iterator.next();
+                if (future.isDone()) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/org/kin/framework/actor/TestActorLike.java
@@ -0,0 +1,34 @@
+package org.kin.framework.actor;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author huangjianqin
+ * @date 2019/7/9
+ */
+public class TestActorLike {
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(1);
+        SimpleActorLike actorLike = new SimpleActorLike();
+        actorLike.tell((actor) -> {
+            System.out.println(1);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(2);
+            latch.countDown();
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class SimpleActorLike extends ActorLike<SimpleActorLike>{
+
+    }
+}
