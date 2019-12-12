ca2ed89a853a84d30d8b158d9db7554ba4e35f07
huangjianqin <18814127639@163.com>
2019-12-12 17:50:48 +0800

    增加IP工具类

---
+++ src/test/java/org/kin/framework/utils/IPUtilsTest.java
@@ -0,0 +1,11 @@
+package org.kin.framework.utils;
+
+/**
+ * @author huangjianqin
+ * @date 2019-12-12
+ */
+public class IPUtilsTest {
+    public static void main(String[] args) {
+        System.out.println(IPUtils.getIp());
+    }
+}
