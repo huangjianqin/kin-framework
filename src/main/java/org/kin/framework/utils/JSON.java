7dec4c3bf9ddb547d665292537953a7b2dc06cf2
huangjianqin <18814127639@163.com>
2019-12-28 13:44:47 +0800

    新增JSON工具类

---
+++ src/main/java/org/kin/framework/utils/JSON.java
@@ -0,0 +1,14 @@
+package org.kin.framework.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author huangjianqin
+ * @date 2019-12-28
+ */
+public class JSON {
+    public static final ObjectMapper parser = new ObjectMapper();
+
+    private JSON() {
+    }
+}
