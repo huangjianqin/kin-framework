fe396a5dc4dd279a15ef28b58f0c24043e8fd30e
huangjianqin <18814127639@163.com>
2020-02-06 17:29:25 +0800

    异常工具类增加获取异常信息

--- src/main/java/org/kin/framework/utils/ExceptionUtils.java
+++ src/main/java/org/kin/framework/utils/ExceptionUtils.java
@@ -3,6 +3,9 @@ package org.kin.framework.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * Created by huangjianqin on 2018/1/28.
  */
@@ -19,5 +22,12 @@ public class ExceptionUtils {
 
     public static void log(String msg, Object... params) {
         log.error(msg, params);
+    }
+
+    public static String getExceptionDesc(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
     }
 }
