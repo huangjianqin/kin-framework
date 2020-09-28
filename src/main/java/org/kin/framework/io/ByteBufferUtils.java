97005c82809b0b63ee55c9e4a32533f0e3345f62
huangjianqin <18814127639@163.com>
2020-09-28 16:53:48 +0800

    新增ByteBuffer工具类

---
+++ src/main/java/org/kin/framework/io/ByteBufferUtils.java
@@ -0,0 +1,64 @@
+package org.kin.framework.io;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author huangjianqin
+ * @date 2020/9/28
+ */
+public class ByteBufferUtils {
+    /**
+     * @return 是否是读模式
+     */
+    public static boolean isReadMode(ByteBuffer target) {
+        return !isWriteMode(target);
+    }
+
+    /**
+     * @return 是否是写模式
+     */
+    public static boolean isWriteMode(ByteBuffer target) {
+        return target.position() != 0 || target.limit() == target.capacity();
+    }
+
+    /**
+     * 切换为读模式
+     * <p>
+     * {@param target}本身要处于write mode, 不然存在数据丢失
+     * 比如read了一点点数据, 再调用该方法, buffer本身只能再读之前读过的数据
+     */
+    public static void toReadMode(ByteBuffer target) {
+        if (!isReadMode(target)) {
+            //change read
+            target.flip();
+        }
+    }
+
+    /**
+     * 切换为写模式
+     */
+    public static void toWriteMode(ByteBuffer target) {
+        if (!isWriteMode(target)) {
+            target.position(target.limit());
+            target.limit(target.capacity());
+        }
+    }
+
+    /**
+     * 将{@param src}内容复制到{@param target}
+     */
+    public static void copy(ByteBuffer target, ByteBuffer src) {
+        toWriteMode(target);
+        toReadMode(src);
+
+        target.put(src);
+    }
+
+    /**
+     * 将{@param src}内容复制到{@param target}, 并清掉{@param src}内容
+     */
+    public static void copyAndClear(ByteBuffer target, ByteBuffer src) {
+        copy(target, src);
+        src.clear();
+    }
+}
