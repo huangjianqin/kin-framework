31f67b33c331fc8221be0f2e7b0d7ba956f0370c
huangjianqin <18814127639@163.com>
2021-05-16 15:30:07 +0800

    properties工具类优化

--- kin-framework-java/src/main/java/org/kin/framework/utils/PropertiesUtils.java
+++ kin-framework-java/src/main/java/org/kin/framework/utils/PropertiesUtils.java
@@ -3,6 +3,7 @@ package org.kin.framework.utils;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -10,7 +11,12 @@ import java.util.Properties;
  * @date 2019/7/6
  */
 public class PropertiesUtils {
-    public static Properties loadPropertie(String propertyFileName) {
+    /**
+     * 读取properties文件
+     * 如果字符串是file:开头, 则是基于相对路径读取properties文件
+     * 否则从class path读取properties文件
+     */
+    public static Properties loadProperties(String propertyFileName) {
         // disk path
         if (propertyFileName.startsWith("file:")) {
             propertyFileName = propertyFileName.substring("file:".length());
@@ -20,6 +26,9 @@ public class PropertiesUtils {
         }
     }
 
+    /**
+     * 从class path读取properties文件
+     */
     public static Properties loadClassPathProperties(String propertyFileName) {
         InputStream in = null;
         try {
@@ -46,7 +55,9 @@ public class PropertiesUtils {
         throw new IllegalStateException("encounter unknown error");
     }
 
-
+    /**
+     * 基于相对路径读取properties文件
+     */
     public static Properties loadFileProperties(String propertyFileName) {
         InputStream in = null;
         try {
@@ -77,7 +88,17 @@ public class PropertiesUtils {
         throw new IllegalStateException("encounter unknown error");
     }
 
-    public static boolean writeFileProperties(Properties properties, String filePathName) {
+    /**
+     * 基于相对路径写properties文件
+     */
+    public static boolean writeProperties(Properties properties, String filePathName) {
+        return writeProperties(properties, filePathName, null);
+    }
+
+    /**
+     * 基于相对路径写properties文件
+     */
+    public static boolean writeProperties(Properties properties, String filePathName, String comment) {
         FileOutputStream out = null;
         try {
 
@@ -89,7 +110,7 @@ public class PropertiesUtils {
 
             // write data
             out = new FileOutputStream(file, false);
-            properties.store(new OutputStreamWriter(out, StandardCharsets.UTF_8), null);
+            properties.store(new OutputStreamWriter(out, StandardCharsets.UTF_8), comment);
             return true;
         } catch (IOException e) {
             ExceptionUtils.throwExt(e);
@@ -104,5 +125,50 @@ public class PropertiesUtils {
         }
 
         return false;
+    }
+
+    /**
+     * 加载properties内容
+     */
+    public static Properties loadPropertiesContent(String content) {
+        return loadPropertiesContent(null, content);
+    }
+
+    /**
+     * 加载properties内容, 添加到目标properties, 或者新建一个properties
+     */
+    public static Properties loadPropertiesContent(Properties target, String content) {
+        if (Objects.isNull(target)) {
+            target = new Properties();
+        }
+        try {
+            target.load(new StringReader(content));
+        } catch (IOException e) {
+            ExceptionUtils.throwExt(e);
+        }
+        return target;
+    }
+
+    /**
+     * 将properties转换成properties content string
+     */
+    public static String writePropertiesContent(Properties target) {
+        return writePropertiesContent(target, null);
+    }
+
+    /**
+     * 将properties转换成properties content string
+     */
+    public static String writePropertiesContent(Properties target, String comment) {
+        try (StringWriter sw = new StringWriter();
+             PrintWriter pw = new PrintWriter(sw)) {
+            target.store(pw, comment);
+
+            return sw.toString();
+        } catch (Exception e) {
+            ExceptionUtils.throwExt(e);
+        }
+
+        return "";
     }
 }
