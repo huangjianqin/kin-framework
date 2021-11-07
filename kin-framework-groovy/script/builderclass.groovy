c1cb6aeafec8ded49db21f085ed95cd0e5532c1e
huangjianqin <18814127639@163.com>
2021-11-07 17:10:22 +0800

    1. 生成builder类中, boolean类型的builder方法, 参数为空, 默认设置对应字段为true 2. 删除纯builder方法脚本, 可转为使用idea自带的

--- kin-framework-groovy/script/builderclass.groovy
+++ kin-framework-groovy/script/builderclass.groovy
@@ -11,13 +11,13 @@ import java.util.stream.Collectors
  * @date 2020/12/30
  */
 //首字母小写转换
- def firstLowerCase(String s) {
-     char[] chars = s.toCharArray();
-     if (chars[0] >= 'A' && chars[0] <= 'Z') {
-         chars[0] = (char) (chars[0] + 32);
-     }
-     return new String(chars);
- }
+def firstLowerCase(String s) {
+    char[] chars = s.toCharArray();
+    if (chars[0] >= 'A' && chars[0] <= 'Z') {
+        chars[0] = (char) (chars[0] + 32);
+    }
+    return new String(chars);
+}
 
 def builder = new JavaProjectBuilder()
 builder.setEncoding("UTF-8")
@@ -31,12 +31,12 @@ def className = clazz.getSimpleName()
 //泛型参数
 StringJoiner sj = new StringJoiner(",")
 def typeParameters = clazz.getTypeParameters()
-for(typeParameter in typeParameters) {
+for (typeParameter in typeParameters) {
     sj.add(typeParameter.getName())
 }
 //钻石符号描述
 def typeParameterStr = "";
-if(typeParameters != null && typeParameters.size() > 0) {
+if (typeParameters != null && typeParameters.size() > 0) {
     typeParameterStr = "<" + sj.toString() + ">"
     //带泛型参数的类名
     className = className + typeParameterStr
@@ -62,10 +62,20 @@ sb.append("public static class Builder" + typeParameterStr + " {\r\n")
 sb.append(String.format("        private final %s %s = new %s();\r\n\r\n", className, firstLowerCaseClassName, className))
 //生成字段赋值方法
 for (field in fieldName2Types) {
-    sb.append(String.format("        public %s %s(%s %s){\r\n", "Builder" + typeParameterStr, field.getKey(), field.getValue(), field.getKey()))
-            .append(String.format("                %s.%s = %s;\r\n", firstLowerCaseClassName, field.getKey(), field.getKey()))
-            .append("                return this;\r\n")
-            .append("        }\r\n")
+    String fieldType = field.getValue()
+
+    if (fieldType.toLowerCase().contains("boolean")) {
+        //如果类型是boolean, 则字段方法没有参数, 方法逻辑是将该字段设置为true, 当然有些情况是关闭某种功能的, 比如disableXX, 则需要使用者手动修改了
+        sb.append(String.format("        public %s %s(%s %s){\r\n", "Builder" + typeParameterStr, field.getKey(), "",""))
+                .append(String.format("                %s.%s = %s;\r\n", firstLowerCaseClassName, field.getKey(), "true"))
+                .append("                return this;\r\n")
+                .append("        }\r\n")
+    } else {
+        sb.append(String.format("        public %s %s(%s %s){\r\n", "Builder" + typeParameterStr, field.getKey(), field.getValue(), field.getKey()))
+                .append(String.format("                %s.%s = %s;\r\n", firstLowerCaseClassName, field.getKey(), field.getKey()))
+                .append("                return this;\r\n")
+                .append("        }\r\n")
+    }
 }
 //生成build方法
 sb.append(String.format("        public %s build(){\r\n", className))
