1b977f7d1976fb702538df7790e041e555b64ee6
huangjianqin <18814127639@163.com>
2021-11-28 00:19:01 +0800

    新增获取变长整形bytes长度工具方法

--- kin-framework-java/src/main/java/org/kin/framework/utils/VarIntUtils.java
+++ kin-framework-java/src/main/java/org/kin/framework/utils/VarIntUtils.java
@@ -289,4 +289,52 @@ public final class VarIntUtils {
     }
 
     //-----------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * 计算32位变长正数占用的bytes数
+     * Compute the number of bytes that would be needed to encode a varInt. {@code value} is treated as unsigned, so it
+     * won't be sign-extended if negative.
+     */
+    public static int computeRawVarInt32Size(int value) {
+        if ((value & (~0 << 7)) == 0) {
+            return 1;
+        }
+        if ((value & (~0 << 14)) == 0) {
+            return 2;
+        }
+        if ((value & (~0 << 21)) == 0) {
+            return 3;
+        }
+        if ((value & (~0 << 28)) == 0) {
+            return 4;
+        }
+        return 5;
+    }
+
+    /**
+     * 计算64位变长正数占用的bytes数
+     */
+    public static int computeRawVarInt64Size(long value) {
+        // Handle two popular special cases up front ...
+        if ((value & (~0L << 7)) == 0L) {
+            return 1;
+        }
+        if (value < 0L) {
+            return 10;
+        }
+        // ... leaving us with 8 remaining, which we can divide and conquer
+        int n = 2;
+        if ((value & (~0L << 35)) != 0L) {
+            n += 4;
+            value >>>= 28;
+        }
+        if ((value & (~0L << 21)) != 0L) {
+            n += 2;
+            value >>>= 14;
+        }
+        if ((value & (~0L << 14)) != 0L) {
+            n += 1;
+        }
+        return n;
+    }
 }
