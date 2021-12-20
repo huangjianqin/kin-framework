5e61d65dcb0cac182e415eda6bc9d47beee99265
huangjianqin <18814127639@163.com>
2021-12-20 23:34:18 +0800

    新增全局扫描class接口

--- kin-framework-java/src/main/java/org/kin/framework/utils/ClassScanUtils.java
+++ kin-framework-java/src/main/java/org/kin/framework/utils/ClassScanUtils.java
@@ -21,11 +21,25 @@ public final class ClassScanUtils {
     }
 
     /**
-     * 扫描指定package下,
+     * 扫描所有类,
      * 1. 指定实现类
      * 2. 带注解的类
      * 3. 指定父类继承子类
      * 并执行指定class处理逻辑
+     *
+     * @param claxx    接口 | 注解 | 父类
+     * @param consumer 扫描到的class的处理逻辑
+     */
+    public static <T> void scan(Class<T> claxx, Consumer<Class<?>> consumer) {
+        scan("*", claxx, consumer);
+    }
+
+    /**
+     * 扫描指定package下,
+     * 1. 指定实现类
+     * 2. 带注解的类
+     * 3. 指定父类继承子类
+     * 并执行{@code consumer}逻辑
      *
      * @param pkg      包名
      * @param claxx    接口 | 注解 | 父类
@@ -44,6 +58,19 @@ public final class ClassScanUtils {
                 .scan()) {
             getClassInfoList(claxx, scanResult).forEach(ci -> consumer.accept(ci.loadClass()));
         }
+    }
+
+    /**
+     * 扫描所有类,
+     * 1. 指定实现类
+     * 2. 带注解的类
+     * 3. 指定父类继承子类
+     * 并返回
+     *
+     * @param claxx 接口 | 注解 | 父类
+     */
+    public static <T> List<Class<?>> scan(Class<T> claxx) {
+        return scan("*", claxx);
     }
 
     /**
@@ -92,7 +119,21 @@ public final class ClassScanUtils {
      * 1. 指定实现类
      * 2. 带注解的类
      * 3. 指定父类继承子类
-     * 并执行指定class处理逻辑
+     * 并执行{@code consumer}逻辑
+     *
+     * @param claxx    接口 | 注解 | 父类
+     * @param consumer 扫描到的class的处理逻辑
+     */
+    public static <T> void scanAsync(Class<T> claxx, Consumer<Class<?>> consumer) {
+        scanAsync("*", claxx, consumer);
+    }
+
+    /**
+     * 异步扫描所有类,
+     * 1. 指定实现类
+     * 2. 带注解的类
+     * 3. 指定父类继承子类
+     * 并执行{@code consumer}逻辑
      *
      * @param pkg      包名
      * @param claxx    接口 | 注解 | 父类
@@ -103,11 +144,25 @@ public final class ClassScanUtils {
     }
 
     /**
+     * 异步扫描所有类,
+     * 1. 指定实现类
+     * 2. 带注解的类
+     * 3. 指定父类继承子类
+     * 并执行{@code consumer}逻辑
+     *
+     * @param claxx    接口 | 注解 | 父类
+     * @param consumer 扫描到的class的处理逻辑
+     */
+    public static <T> void scanAsync(Class<T> claxx, Consumer<Class<?>> consumer, ClassGraph.FailureHandler failureHandler) {
+        scanAsync("*", claxx, consumer, failureHandler);
+    }
+
+    /**
      * 异步扫描指定package下,
      * 1. 指定实现类
      * 2. 带注解的类
      * 3. 指定父类继承子类
-     * 并执行指定class处理逻辑
+     * 并执行{@code consumer}逻辑
      *
      * @param pkg      包名
      * @param claxx    接口 | 注解 | 父类
