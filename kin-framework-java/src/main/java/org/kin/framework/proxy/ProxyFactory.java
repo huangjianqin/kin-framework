20c8112ced329d6b691ae9989023098c7155bbfd
huangjianqin <18814127639@163.com>
2022-01-01 23:02:38 +0800

    去掉无用unchecked

--- kin-framework-java/src/main/java/org/kin/framework/proxy/ProxyFactory.java
+++ kin-framework-java/src/main/java/org/kin/framework/proxy/ProxyFactory.java
@@ -10,12 +10,10 @@ public interface ProxyFactory {
     /**
      * 增强代理方法
      */
-    @SuppressWarnings("unchecked")
     <T> ProxyInvoker<T> enhanceMethod(MethodDefinition<T> definition);
 
     /**
      * 增强代理类(接口)
      */
-    @SuppressWarnings("unchecked")
     <P> P enhanceClass(ClassDefinition<P> definition);
 }
